c8af268648d605721c63ad6488ec19ebb7c02846
Etem Oğulcan AKPINAR <eoakpinar@icloud.com>
2023-07-26 00:10:38 +0300

    mongodb

---
+++ models/User.js
@@ -0,0 +1,43 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    job: {
+      type: String,
+    },
+    bio: {
+      type: String,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    confirmPassword: {
+      type: String,
+      required: true,
+    },
+    emailVerified: {
+        type: String,
+        default: null
+    }
+  },
+  { timestamps: true }
+);
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
